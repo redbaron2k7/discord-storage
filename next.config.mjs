aec09e5289d7fbeeefbc916b13e8e3722c1cc234
redbaron2k7 <76609332+redbaron2k7@users.noreply.github.com>
2024-08-06 19:22:19 -0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,12 @@ const nextConfig = {
     experimental: {
       appDir: true,
     },
-  }
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.optimization.splitChunks = false;
+        }
+        return config;
+      },
+    }
 
 export default nextConfig;
